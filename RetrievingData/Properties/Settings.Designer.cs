95dd77a67fdfec6a8767379dc12349267fbd795c
nursakinah24 <nursakinah2402@gmail.com>
2022-03-30 15:24:09 +0700

    Konfigurasi sambungan windows form dengan database

--- RetrievingData/Properties/Settings.Designer.cs
+++ RetrievingData/Properties/Settings.Designer.cs
@@ -8,21 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace RetrievingData.Properties
-{
+namespace RetrievingData.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-KINALAKA\\NURSAKINAH;Initial Catalog=AdventureWorks;Persist Sec" +
+            "urity Info=True;User ID=sa;Password=Palu24072002")]
+        public string AdventureWorksConnectionString {
+            get {
+                return ((string)(this["AdventureWorksConnectionString"]));
             }
         }
     }
